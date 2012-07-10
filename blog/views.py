74b36f2e52faf18b28f7807c84aca587f8361b9f
=Seth Fiawoo <moi@fiawoo-PC.(none)>
2012-07-10 15:59:09 +0000

    Blog with template

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from models import Post, Comment
 def post_list(request):
     post_list = Post.objects.all()
     
-    t=loader.get_template('blog/all_posts.html')
+    t=loader.get_template('blog/post_list.html')
     c=Context({'post_list':post_list,})
     #return HttpResponse(post_list)
     return HttpResponse(t.render(c))
@@ -28,8 +28,12 @@ def post_detail(request, id, showComments=False):
     
 def post_search(request, term):
     post=Post.objects.filter(body__contains=term)
-    return HttpResponse(post)
+    t=loader.get_template('blog/post_search.html')
+    c=Context({'post_list':post,'search':term})
+    return HttpResponse(t.render(c))
 
 def home(request):
     print 'it works'
-    return HttpResponse('hello world. Ete zene?') 
+    t=loader.get_template('base.html')
+    c=Context({})
+    return HttpResponse(t.render(c)) 
